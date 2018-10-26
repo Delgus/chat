fc295dcdd20cd3e29e39b8081dd95614d38c735c
Alex <alexdolgovak47@mail.ru>
2018-10-26 15:13:52 +0300

    fix path

--- auth/bin/auth-install.php
+++ auth/bin/auth-install.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__.'/../../config/config-local.php';
-$pdo = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+require_once __DIR__ . '/../../config/config-local.php';
+$pdo = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
 //create users table
 $pdo->exec(
-	"CREATE TABLE `users`(
+    "CREATE TABLE `users`(
  			  		`id` INT NOT NULL AUTO_INCREMENT ,
  			  		`username` TEXT NOT NULL , 
  			  		`email` TEXT NOT NULL , 
--- chat/bin/server.php
+++ chat/bin/server.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../config/config-local.php';
 
 use Firebase\JWT\JWT;
